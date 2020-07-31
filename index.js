6bc1bf9af408a5dec4dd9cb76190f6860fe4e6e0
azzahraa <azzahraa36@gmail.com>
2020-07-31 21:03:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let {pets}  = { 
+    pets :{
+    name: "Max", type: "dog", bornOn: 2018 ,
+    name: "Angel", type: "cat", bornOn: 2015 ,
+    name: "Jasper", type: "dog", bornOn: 2016 }
+};
+
+const getAge = (pet) =>  new Date().getFullYear() - pet.bornOn;
+
+
+pets.forEach((ele) => {
+    ele.age = getAge(ele);
+    petsWithAge.push(ele);
+    });
+console.log(petsWithAge);
+
+let dogs = pets.filter((ele) => ele.type === "dog");
+let jasper = pets.find((ele) => ele.name === "Jasper");
+console.log(`Jasper is ${jasper.age} years old`);
